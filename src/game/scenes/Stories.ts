313005f168f89c7ae2747ef2ba43bb4dec956f1e
Manish Devgan <manish.nsit8@gmail.com>
2025-11-13 20:18:33 +0100

    Dev: Cleanup Stories Code
    Todo:
    - Add Sounds
    - Add Logging
    - Add Tests
    - Fix textures appearing different for player in level and intro

--- src/game/scenes/Stories.ts
+++ src/game/scenes/Stories.ts
@@ -1,21 +1,14 @@
-import {StoryScene} from './StoryScene.ts';
 import {getStoryName, LevelNames} from './lib/LevelNames.ts';
 import {GameObject} from '../core/GameObject';
 import {AppColors, phaserColor} from '../scripts/Colors';
 import {VisualPulse} from '../components/VisualPulse';
 import {HEIGHT, WIDTH} from '../scripts/Util';
-import {StoryStep} from "./lib/BaseStoryScene.ts";
+import {BaseStoryScene, StoryStep} from "./lib/BaseStoryScene.ts";
 
-export class StoryLevel1 extends StoryScene {
+export class StoryLevel1 extends BaseStoryScene {
     constructor() {
         super(
-            getStoryName(LevelNames.HelloGenie),
-            [
-                'Even a Guardian needs a guide\nHi I am Genie!',
-                "Static's operations are rising rapidly,\nHe is sending out fleet of Glitches.",
-                'We need your help Guardian!',
-            ],
-            LevelNames.HelloGenie
+            getStoryName(LevelNames.HelloGenie)
         );
     }
 
@@ -74,15 +67,10 @@ export class StoryLevel1 extends StoryScene {
     }
 }
 
-export class StoryLevel2 extends StoryScene {
+export class StoryLevel2 extends BaseStoryScene {
     constructor() {
         super(
-            getStoryName(LevelNames.TrustMe),
-            [
-                "The Glitches have identified more paths to the nexus\nIt's not looking good.",
-                "But I can still count on you, right?",
-            ],
-            LevelNames.TrustMe
+            getStoryName(LevelNames.TrustMe)
         );
     }
 
@@ -129,17 +117,10 @@ export class StoryLevel2 extends StoryScene {
     }
 }
 
-export class StoryLevel3 extends StoryScene {
+export class StoryLevel3 extends BaseStoryScene {
     constructor() {
         super(
-            getStoryName(LevelNames.ThePhantom),
-            [
-                'I have some news for you Guardian.',
-                'The Phantom is on the move!',
-                'Remember Guardian:\n\n- It disables all towers\n- Only you can defeat the Phantom!',
-                'Good Luck Guardian!',
-            ],
-            LevelNames.ThePhantom
+            getStoryName(LevelNames.ThePhantom)
         );
     }
 
@@ -190,18 +171,10 @@ export class StoryLevel3 extends StoryScene {
     }
 }
 
-export class StoryLevel4 extends StoryScene {
+export class StoryLevel4 extends BaseStoryScene {
     constructor() {
         super(
-            getStoryName(LevelNames.RiseOfStatic),
-            [
-                "We don't know the source of Static's Power",
-                'But with your help we can sample the remains of Phantom.',
-                'And maybe we can put an end to it all!',
-                'Oh no, there are more glitches!!',
-                'You are needed Guardian!',
-            ],
-            LevelNames.RiseOfStatic
+            getStoryName(LevelNames.RiseOfStatic)
         );
     }
 
@@ -264,16 +237,10 @@ export class StoryLevel4 extends StoryScene {
     }
 }
 
-export class StoryLevel5 extends StoryScene {
+export class StoryLevel5 extends BaseStoryScene {
     constructor() {
         super(
-            getStoryName(LevelNames.Breakthrough),
-            [
-                'That was amazing, I know we can count on you!',
-                'Also, I am close to sampling Phantom\nI need maybe 2 more for a breakthrough',
-                'Can you do that, Guardian?',
-            ],
-            LevelNames.Breakthrough
+            getStoryName(LevelNames.Breakthrough)
         );
     }
 
@@ -310,17 +277,10 @@ export class StoryLevel5 extends StoryScene {
     }
 }
 
-export class StoryLevel6 extends StoryScene {
+export class StoryLevel6 extends BaseStoryScene {
     constructor() {
         super(
             getStoryName(LevelNames.TheCliffhanger),
-            [
-                'Even the Genie couldn\'t figure out',
-                'But the developer might!',
-                'Please reach out if you want the story to proceed...',
-                ''
-            ],
-            'MenuScene'
         );
     }
 
@@ -336,11 +296,8 @@ export class StoryLevel6 extends StoryScene {
                     text: 'But the developer might!',
                 },
                 {
-                    text: 'Please reach out if you want the story to proceed...',
+                    text: 'Please reach out if you would like the story to proceed...',
                 },
-                {
-                    text: ''
-                }
             ],
         };
     }
